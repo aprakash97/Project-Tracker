5f44e52fadcc5a345ce57ec709231a62faa3e544
Prakash <prakashakrakr@gmail.com>
2023-03-11 15:20:06 +0530

    route tested successfully

---
+++ src/app/api/hello/(auth)/layout.tsx
@@ -0,0 +1,29 @@
+
+// import GlassPane from "@/components/GlassPane";
+
+// export default function AuthRootLayout({children}){
+//     return(
+//         <html lang="en">
+//             <body className="h-screen w-screen rainbow-mesh p-6">
+//                 <GlassPane className="w-full h-full flex items-center">
+//                     {children}
+//                 </GlassPane>
+//             </body>
+//         </html>
+//     )
+
+// }
+import GlassPane from "@/components/GlassPane";
+
+export default function AuthRootLayout({children}) {
+  return (
+    <html lang="en">
+      <head />
+      <body className="h-screen w-screen rainbow-mesh p-6">
+        <GlassPane className="w-full h-full flex items-center justify-center">
+          {children}
+        </GlassPane>
+      </body>
+    </html>
+  )
+}
---
+++ src/app/api/hello/(auth)/signin/page.tsx
@@ -0,0 +1,11 @@
+export default function Signin(){
+    return(
+        <div>
+            <p> signin </p>
+            <h1>saddsadsa</h1>
+        </div>
+    )
+}
+
+
+// CHECK SRC FOLDER STRUCTURE AGAINST THE VIDEO
